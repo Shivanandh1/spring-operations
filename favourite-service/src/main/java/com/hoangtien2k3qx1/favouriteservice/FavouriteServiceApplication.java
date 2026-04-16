53a8d6c89f6602e3960e6630c2d137b254415ba2
shivnanandh <shivanandh1233@gmail.com>
2026-04-16 16:41:21 +0530

    added files:

---
+++ favourite-service/src/main/java/com/hoangtien2k3qx1/favouriteservice/FavouriteServiceApplication.java
@@ -0,0 +1,18 @@
+package com.hoangtien2k3qx1.favouriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.TimeZone;
+
+@SpringBootApplication(scanBasePackages = "com.hoangtien2k3")
+@EnableDiscoveryClient
+public class FavouriteServiceApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT +0:00"));
+		SpringApplication.run(FavouriteServiceApplication.class, args);
+	}
+
+}
---
+++ notification-service/src/main/java/com/hoangtien2k3/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hoangtien2k3.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = "com.hoangtien2k3")
+@EnableDiscoveryClient
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
